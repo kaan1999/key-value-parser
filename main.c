09f0e46f46a13fd110bc96660db6ddedd02445f4
Ahmet Kaan <ahmet.kaan.yalcinkaya99@gmail.com>
2024-08-30 21:45:25 +0300

    reset

--- main.c
+++ main.c
@@ -1,10 +1,4 @@
-#include <kv_list.h>
-#include <stdio.h>
+
 int main(void){
-    //kvpair_init and kvpair_destroy test
-    kvpair_ptr pair = kvpair_init("key1", "value1");
-    if(pair){
-        puts("OK");
-    }
-    kvpair_destroy(pair);
+
 }
