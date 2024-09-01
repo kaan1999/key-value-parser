0bb062eb382e2cb091cc887c63fb3a06b541404b
Ahmet Kaan <ahmet.kaan.yalcinkaya99@gmail.com>
2024-09-01 17:11:15 +0300

    implement kv_list.h

---
+++ inc/kv_list.h
@@ -0,0 +1,9 @@
+#include <errno.h>
+typedef struct KVNode kv_node_t, kv_node_ptr;
+typedef struct KVList kv_list_t, kv_list_ptr;
+
+kv_node_ptr kv_node_init(void *data);
+errno_t kv_node_destroy(kv_node_ptr node, errno_t (*funDestroy)(void*));
+kv_list_ptr kv_list_init();
+errno_t kv_list_destroy(kv_list_ptr list, errno_t (*funDestroy)(void*));
+errno_t kv_list_push(kv_list_ptr list, void *data);
